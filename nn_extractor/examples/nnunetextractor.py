de84403429886dd1a62f4c6e1f07094d43383b7c
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-03-03 18:42:51 -0500

    add original nnUNetPredictor._internal_maybe_mirror_and_predict

--- nn_extractor/examples/nnunetextractor.py
+++ nn_extractor/examples/nnunetextractor.py
@@ -1,3 +1,4 @@
+import itertools
 from queue import Queue
 from threading import Thread
 from typing import Optional, Union
@@ -184,6 +185,25 @@ class nnUNetPredictor(baseNNUNetPredictor):
 
         if self.verbose: print('Prediction done')  # fmt: off  # noqa
         torch.set_num_threads(n_threads)
+        return prediction
+
+    @torch.inference_mode()
+    def _internal_maybe_mirror_and_predict(self, x: torch.Tensor) -> torch.Tensor:
+        mirror_axes = self.allowed_mirroring_axes if self.use_mirroring else None
+        prediction = self.network(x)
+
+        if mirror_axes is not None:
+            # check for invalid numbers in mirror_axes
+            # x should be 5d for 3d images and 4d for 2d. so the max value of mirror_axes cannot exceed len(x.shape) - 3
+            assert max(mirror_axes) <= x.ndim - 3, 'mirror_axes does not match the dimension of the input!'
+
+            mirror_axes = [m + 2 for m in mirror_axes]
+            axes_combinations = [
+                c for i in range(len(mirror_axes)) for c in itertools.combinations(mirror_axes, i + 1)
+            ]
+            for axes in axes_combinations:
+                prediction += torch.flip(self.network(torch.flip(x, axes)), axes)
+            prediction /= (len(axes_combinations) + 1)
         return prediction
 
     @torch.inference_mode()
