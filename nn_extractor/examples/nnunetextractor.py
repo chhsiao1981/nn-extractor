4c67999524ec91ca22043fdc420557cdf225f7ae
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-03-03 19:15:13 -0500

    nnunetextractor: add predict_entry_point

--- nn_extractor/examples/nnunetextractor.py
+++ nn_extractor/examples/nnunetextractor.py
@@ -1,9 +1,14 @@
 import itertools
+import os
 from queue import Queue
+import re
 from threading import Thread
 from typing import Optional, Self, Union
 
-from nn_extractor import nii
+from pydantic import BaseModel, Field
+
+from nn_extractor import argparse, cfg, nii
+import nn_extractor
 from nn_extractor.nnextractor import NNExtractor
 from nn_extractor.ops.crop import Crop
 from nn_extractor.ops.flip import Flip
@@ -11,17 +16,19 @@ from nn_extractor.ops.pad import Pad
 import numpy as np
 import torch
 from acvl_utils.cropping_and_padding.padding import pad_nd_image
-from batchgenerators.utilities.file_and_folder_operations import join, maybe_mkdir_p, isdir
+from batchgenerators.utilities.file_and_folder_operations import maybe_mkdir_p, isdir
 from torch._dynamo import OptimizedModule
 from tqdm import tqdm
 
 from nnunetv2.configuration import default_num_processes
+from nnunetv2.imageio.simpleitk_reader_writer import SimpleITKIO
 from nnunetv2.inference.data_iterators import PreprocessAdapterFromNpy
 from nnunetv2.inference.export_prediction import \
     convert_predicted_logits_to_segmentation_with_correct_shape
 from nnunetv2.inference.sliding_window_prediction import compute_gaussian
 from nnunetv2.utilities.file_path_utilities import get_output_folder
 from nnunetv2.utilities.helpers import empty_cache, dummy_context
+from nnunetv2.utilities.utils import create_lists_from_splitted_dataset_folder
 
 from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor as baseNNUNetPredictor
 
@@ -432,70 +439,120 @@ class nnUNetPredictor(baseNNUNetPredictor):
         return predicted_logits
 
 
-def predict_entry_point():
-    import argparse
+class Args(BaseModel):
+    # https://stackoverflow.com/questions/72741663/argument-parser-from-a-pydantic-model
+    i: str = Field(description='input filename with _0000.nii.gz.')
+    o: str = Field(description='Output folder.')
+    d: str = Field(description='Dataset with which you trained.')
+    p: str = Field(default='nnUNetPlans', description='Plans identifier.')
+    tr: str = Field(alias='tr', default='nnUNetTrainer', description='nnUNet trainer.')
+    c: str = Field(
+        description='nnUNet config (2d, 3d_lowres, 3d_fullres, 3d_cascade_fullres)')
+    f: str = Field(
+        nargs='+',
+        default=(0, 1, 2, 3, 4),
+        description='folds of the trained model')
+    step_size: float = Field(
+        alias='step_size',
+        default=0.5, description='Step size for sliding window prediction.')
+    disable_tta: bool = Field(
+        default=False,
+        action='store_true',
+        description='Set this flag to disable test time data augmentationin the form of mirroring.')
+    verbose: bool = Field(
+        default=False,
+        action='store_true',
+        description='debugging')
+    save_probabilities: bool = Field(
+        default=False,
+        action='store_true',
+        description='export predicted class "probabilities".')
+    continue_prediction: bool = Field(
+        default=False,
+        action='store_true',
+        description='Continue an aborted previous prediction.')
+    chk: str = Field(
+        alias='chk',
+        default='checkpoint_final.pth',
+        description='Name of the checkpoint.')
+    npp: int = Field(
+        alias='npp',
+        default=3,
+        description='Number of processes used for preprocessing.')
+    nps: int = Field(
+        alias='nps',
+        default=3,
+        description='Number of processes used for segmentation export.')
+    prev_stage_predictions: str = Field(
+        alias='prev_stage_predictions',
+        default=None,
+        description='Folder containing the predictions of the previous stage.'
+        'Required for cascaded models.')
+    device: str = Field(
+        alias='device',
+        default='cuda',
+        description='set the device the inference should run with. (cuda, cpu, mps).'
+        'Set GPU ID as environment variable CUDA_VISIBLE_DEVICES=X if needed.')
+    disable_progress_bar: bool = Field(
+        default=False,
+        action='store_true',
+        description='Set this flag to disable progress bar.')
+
+    nn_extractor_name: str = Field(
+        default='nnunetv2',
+        description='nn-extractor name',
+        alias='N')
+
+    nn_extractor_config_file: str = Field(
+        default='',
+        description='nn-extractor config filename',
+        alias='C')
+
+
+def _determine_filenames(filename: str) -> tuple[str, list[str], str]:
+    the_basename = os.path.basename(filename)
+    filename_list = the_basename.split('.')
+    filename0_list = filename_list[0].split('_')
+    identifier = '_'.join(filename0_list[:-1])
+    filename_postfix = '.'.join(filename_list[1:])
+
+    the_dirname = os.path.dirname(filename)
+
+    # https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunetv2/inference/predict_from_raw_data.py#L171
+    list_of_full_filenames = create_lists_from_splitted_dataset_folder(
+        the_dirname, file_ending=f'.{filename_postfix}', identifiers=[identifier], num_processes=1)
+    if len(list_of_full_filenames) != 1:
+        raise Exception(f'_determine_filenames: invalid list_of_full_filenames: filename_prefix: {identifier} full_filenames: {list_of_full_filenames}')  # noqa
+
+    full_filenames = list_of_full_filenames[0]
+
+    # label filename
+    the_dirname_basename = os.path.basename(the_dirname)
+    the_dirname_label_basename = re.sub(r'images', 'labels', the_dirname_basename)
+    the_dirname_dirname = os.path.dirname(the_dirname)
+    label_dirname = os.sep.join([the_dirname_dirname, the_dirname_label_basename])
+
+    base_label_filename = f'{identifier}.{filename_postfix}'
+    full_label_filename = os.sep.join([label_dirname, base_label_filename])
+    if not os.path.exists(full_label_filename):
+        full_label_filename = None
+
+    cfg.logger.info(f'filename: {filename} identifier: {identifier}, full_filenames: {full_filenames} full_label_filename: {full_label_filename}')  # noqa
+
+    return identifier, full_filenames, full_label_filename
+
+
+def parse_args():
     parser = argparse.ArgumentParser(description='Use this to run inference with nnU-Net. This function is used when '  # noqa
                                                  'you want to manually specify a folder containing a trained nnU-Net '  # noqa
                                                  'model. This is useful when the nnunet environment variables '  # noqa
                                                  '(nnUNet_results) are not set.')
-    parser.add_argument('-i', type=str, required=True,
-                        help='input folder. Remember to use the correct channel numberings for your files (_0000 etc). '  # noqa
-                             'File endings must be the same as the training dataset!')
-    parser.add_argument('-o', type=str, required=True,
-                        help='Output folder. If it does not exist it will be created. Predicted segmentations will '  # noqa
-                             'have the same name as their source images.')
-    parser.add_argument('-d', type=str, required=True,
-                        help='Dataset with which you would like to predict. You can specify either dataset name or id')  # noqa
-    parser.add_argument('-p', type=str, required=False, default='nnUNetPlans',
-                        help='Plans identifier. Specify the plans in which the desired configuration is located. '  # noqa
-                             'Default: nnUNetPlans')
-    parser.add_argument('-tr', type=str, required=False, default='nnUNetTrainer',
-                        help='What nnU-Net trainer class was used for training? Default: nnUNetTrainer')  # noqa
-    parser.add_argument('-c', type=str, required=True,
-                        help='nnU-Net configuration that should be used for prediction. Config must be located '  # noqa
-                             'in the plans specified with -p')
-    parser.add_argument('-f', nargs='+', type=str, required=False, default=(0, 1, 2, 3, 4),
-                        help='Specify the folds of the trained model that should be used for prediction. '  # noqa
-                             'Default: (0, 1, 2, 3, 4)')
-    parser.add_argument('-step_size', type=float, required=False, default=0.5,
-                        help='Step size for sliding window prediction. The larger it is the faster but less accurate '  # noqa
-                             'the prediction. Default: 0.5. Cannot be larger than 1. We recommend the default.')  # noqa
-    parser.add_argument('--disable_tta', action='store_true', required=False, default=False,
-                        help='Set this flag to disable test time data augmentation in the form of mirroring. Faster, '  # noqa
-                             'but less accurate inference. Not recommended.')
-    parser.add_argument('--verbose', action='store_true', help="Set this if you like being talked to. You will have "  # noqa
-                                                               "to be a good listener/reader.")
-    parser.add_argument('--save_probabilities', action='store_true',
-                        help='Set this to export predicted class "probabilities". Required if you want to ensemble '  # noqa
-                             'multiple configurations.')
-    parser.add_argument('--continue_prediction', action='store_true',
-                        help='Continue an aborted previous prediction (will not overwrite existing files)')  # noqa
-    parser.add_argument('-chk', type=str, required=False, default='checkpoint_final.pth',
-                        help='Name of the checkpoint you want to use. Default: checkpoint_final.pth')  # noqa
-    parser.add_argument('-npp', type=int, required=False, default=3,
-                        help='Number of processes used for preprocessing. More is not always better. Beware of '  # noqa
-                             'out-of-RAM issues. Default: 3')
-    parser.add_argument('-nps', type=int, required=False, default=3,
-                        help='Number of processes used for segmentation export. More is not always better. Beware of '  # noqa
-                             'out-of-RAM issues. Default: 3')
-    parser.add_argument('-prev_stage_predictions', type=str, required=False, default=None,
-                        help='Folder containing the predictions of the previous stage. Required for cascaded models.')  # noqa
-    parser.add_argument('-num_parts', type=int, required=False, default=1,
-                        help='Number of separate nnUNetv2_predict call that you will be making. Default: 1 (= this one '  # noqa
-                             'call predicts everything)')
-    parser.add_argument('-part_id', type=int, required=False, default=0,
-                        help='If multiple nnUNetv2_predict exist, which one is this? IDs start with 0 can end with '  # noqa
-                             'num_parts - 1. So when you submit 5 nnUNetv2_predict calls you need to set -num_parts '  # noqa
-                             '5 and use -part_id 0, 1, 2, 3 and 4. Simple, right? Note: You are yourself responsible '  # noqa
-                             'to make these run on separate GPUs! Use CUDA_VISIBLE_DEVICES (google, yo!)')  # noqa
-    parser.add_argument('-device', type=str, default='cuda', required=False,
-                        help="Use this to set the device the inference should run with. Available options are 'cuda' "  # noqa
-                             "(GPU), 'cpu' (CPU) and 'mps' (Apple M1/M2). Do NOT use this to set which GPU ID! "  # noqa
-                             "Use CUDA_VISIBLE_DEVICES=X nnUNetv2_predict [...] instead!")
-    parser.add_argument('--disable_progress_bar', action='store_true', required=False, default=False,  # noqa
-                        help='Set this flag to disable progress bar. Recommended for HPC environments (non interactive '  # noqa
-                             'jobs)')
+    argparse.add_args(parser, Args)
 
+    return parser.parse_args()
+
+
+def predict_entry_point():
     print(
         "\n#######################################################################\nPlease cite the following paper "  # noqa
         "when using nnU-Net:\n"
@@ -503,16 +560,13 @@ def predict_entry_point():
         "nnU-Net: a self-configuring method for deep learning-based biomedical image segmentation. "
         "Nature methods, 18(2), 203-211.\n#######################################################################\n")  # noqa
 
-    args = parser.parse_args()
+    args = parse_args()
     args.f = [i if i == 'all' else int(i) for i in args.f]
 
     model_folder = get_output_folder(args.d, args.tr, args.p, args.c)
 
     if not isdir(args.o):
         maybe_mkdir_p(args.o)
-
-    # slightly passive aggressive haha
-    assert args.part_id < args.num_parts, 'Do you even read the documentation? See nnUNetv2_predict -h.'  # noqa
 
     assert args.device in ['cpu', 'cuda',
                            'mps'], f'-device must be either cpu, mps or cuda. Other devices are not tested/supported. Got: {args.device}.'  # noqa
@@ -529,6 +583,13 @@ def predict_entry_point():
     else:
         device = torch.device('mps')
 
+    # init nn-extractor
+    extra_params: nn_extractor.Config = {}
+    if args.nn_extractor_name:
+        extra_params['name'] = args.nn_extractor_name
+    nn_extractor.init(args.nn_extractor_config_file, extra_params=extra_params)
+    nnextractor_name = nn_extractor.config['name']
+
     predictor = nnUNetPredictor(tile_step_size=args.step_size,
                                 use_gaussian=True,
                                 use_mirroring=not args.disable_tta,
@@ -542,71 +603,27 @@ def predict_entry_point():
         args.f,
         checkpoint_name=args.chk
     )
-    predictor.predict_from_files(args.i, args.o, save_probabilities=args.save_probabilities,
-                                 overwrite=not args.continue_prediction,
-                                 num_processes_preprocessing=args.npp,
-                                 num_processes_segmentation_export=args.nps,
-                                 folder_with_segs_from_prev_stage=args.prev_stage_predictions,
-                                 num_parts=args.num_parts,
-                                 part_id=args.part_id)
-    # r = predict_from_raw_data(args.i,
-    #                           args.o,
-    #                           model_folder,
-    #                           args.f,
-    #                           args.step_size,
-    #                           use_gaussian=True,
-    #                           use_mirroring=not args.disable_tta,
-    #                           perform_everything_on_device=True,
-    #                           verbose=args.verbose,
-    #                           save_probabilities=args.save_probabilities,
-    #                           overwrite=not args.continue_prediction,
-    #                           checkpoint_name=args.chk,
-    #                           num_processes_preprocessing=args.npp,
-    #                           num_processes_segmentation_export=args.nps,
-    #                           folder_with_segs_from_prev_stage=args.prev_stage_predictions,
-    #                           num_parts=args.num_parts,
-    #                           part_id=args.part_id,
-    #                           device=device)
 
+    filename_prefix, filenames, label_filename = _determine_filenames(args.i)
 
-if __name__ == '__main__':
-    ########################## predict a bunch of files  # noqa
-    from nnunetv2.paths import nnUNet_results, nnUNet_raw  # noqa
+    output_filename = os.sep.join([args.o, f'{filename_prefix}.nii.gz'])
 
-    predictor = nnUNetPredictor(
-        tile_step_size=0.5,
-        use_gaussian=True,
-        use_mirroring=True,
-        perform_everything_on_device=True,
-        device=torch.device('cuda', 0),
-        verbose=False,
-        verbose_preprocessing=False,
-        allow_tqdm=True
+    img, props = SimpleITKIO().read_images(filenames)
+
+    label_img: Optional[np.ndarray] = None
+    label_props: Optional[dict] = None
+    if label_filename:
+        label_img, label_props = SimpleITKIO().read_images([label_filename])
+        label_img = label_img[0]
+
+    _ = predictor.predict_single_npy_array(
+        input_image=img,
+        image_properties=props,
+        segmentation_previous_stage=None,
+        output_file_truncated=output_filename,
+        save_or_return_probabilities=args.save_probabilities,
+
+        nnextractor_name=f'{nnextractor_name}-{filename_prefix}',
+        label_image=label_img,
+        label_image_properties=label_props,
     )
-    predictor.initialize_from_trained_model_folder(
-        join(nnUNet_results, 'Dataset004_Hippocampus/nnUNetTrainer_5epochs__nnUNetPlans__3d_fullres'),  # noqa
-        use_folds=(0,),
-        checkpoint_name='checkpoint_final.pth',
-    )
-    # predictor.predict_from_files(join(nnUNet_raw, 'Dataset003_Liver/imagesTs'),
-    #                              join(nnUNet_raw, 'Dataset003_Liver/imagesTs_predlowres'),
-    #                              save_probabilities=False, overwrite=False,
-    #                              num_processes_preprocessing=2, num_processes_segmentation_export=2,  # noqa
-    #                              folder_with_segs_from_prev_stage=None, num_parts=1, part_id=0)
-    #
-    # # predict a numpy array
-    # from nnunetv2.imageio.simpleitk_reader_writer import SimpleITKIO
-    #
-    # img, props = SimpleITKIO().read_images([join(nnUNet_raw, 'Dataset003_Liver/imagesTr/liver_63_0000.nii.gz')])  # noqa
-    # ret = predictor.predict_single_npy_array(img, props, None, None, False)
-    #
-    # iterator = predictor.get_data_iterator_from_raw_npy_data([img], None, [props], None, 1)
-    # ret = predictor.predict_from_data_iterator(iterator, False, 1)
-
-    ret = predictor.predict_from_files_sequential(
-        [['/media/isensee/raw_data/nnUNet_raw/Dataset004_Hippocampus/imagesTs/hippocampus_002_0000.nii.gz'], ['/media/isensee/raw_data/nnUNet_raw/Dataset004_Hippocampus/imagesTs/hippocampus_005_0000.nii.gz']],  # noqa
-        '/home/isensee/temp/tmp', False, True, None
-    )
-
-
-# noqa
