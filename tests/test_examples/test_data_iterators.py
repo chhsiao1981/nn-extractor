5f15e6b36d41e22b75c5ff1897f2557cef756e5f
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-03-12 19:43:40 -0400

    nnunetextractor: add more files from nnunetv2
    nnunetv2/inference/data_iterators.py => nn_extractor/examples/data_iterators.py
    nnunetv2/preprocessing/preprocessors/default_preprocessor.py => nn_extractor/examples/default_preprocessor.py

---
+++ tests/test_examples/test_data_iterators.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import logging  # noqa
+
+from nn_extractor.examples import data_iterators  # noqa
+
+
+class TestDataIterators(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
